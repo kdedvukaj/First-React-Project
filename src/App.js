bc0f1c298cb52acead3c62607b888cd4625b7ddf
kdedvukaj <kdedvukaj94@gmail.com>
2022-10-09 00:42:07 +0200

    Add Route on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
+      <Route path="/new-meetup">
+        <NewMeetupPage />
+      </Route>
+      <Route path="/favourites">
+        <FavouritesPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
