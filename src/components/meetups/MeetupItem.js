472c721a35d59417c0a7567ce95a65a6ef055375
kdedvukaj <kdedvukaj94@gmail.com>
2022-10-09 17:17:19 +0200

    MeetupItem page

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,19 @@
+function MeetupItem() {
+  return (
+    <li>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h3>Title</h3>
+        <address>Address</address>
+        <p>Description</p>
+      </div>
+      <div>
+        <button>To favourites</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
